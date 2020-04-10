4cb72fb8de62e36f6b8e1e9e9b03a450a7b7ad8e
思维空间 <dnyz520@126.com>
2020-04-11 01:27:24 +0800

    up

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,9 @@
-import { name } from "./package.json";
 import typescript from "rollup-plugin-typescript2";
 import { terser } from "rollup-plugin-terser";
 
 export default {
   input: 'src/index.ts',
-  name,
+  name: 'PrintCS',
   output: {
     file: `./index.umd.min.js`,
     format: 'umd',
